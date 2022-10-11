4eb2ddb812bd5da14038e48518c3ff17c79e25a7
Jack Humphries <jackphumphries02@gmail.com>
2022-10-11 13:59:42 +0100

    Implemented Stack but needs more test coverage

--- DataStructures.Tests/StackTest.cs
+++ DataStructures.Tests/StackTest.cs
@@ -3,11 +3,37 @@ namespace DataStructures.Tests;
 public class StackTest
 {
     [Fact]
-    public void Test1()
+    public void TestPush()
     {
-        var temp = new DataStructures.Stack();
-        var res = temp.add(2, 4);
+        DataStructures.Stack<int> teststack = new DataStructures.Stack<int>();
 
-        Assert.True(res == 6, "2 + 4 should equal 6");
+        teststack.Push(10);
+        Assert.True(teststack.Peek() == 10, "Should add item and peek the item");
+    }
+
+    [Fact]
+    public void TestPeek()
+    {
+        DataStructures.Stack<int> teststack = new DataStructures.Stack<int>();
+
+        teststack.Push(10);
+        teststack.Push(20);
+        teststack.Push(30);
+
+        Assert.True(teststack.Peek() == 30, "Should be the last item in which is 30");
+    }
+
+    [Fact]
+    public void TestPop()
+    {
+        DataStructures.Stack<int> teststack = new DataStructures.Stack<int>();
+
+        teststack.Push(10);
+        teststack.Push(20);
+        teststack.Push(30);
+
+        teststack.Pop();
+
+        Assert.True(teststack.Peek() == 20);
     }
 }
--- DataStructures/Stack.cs
+++ DataStructures/Stack.cs
@@ -1,6 +1,45 @@
 ﻿namespace DataStructures;
-public class Stack
+public class Stack<T>
 {
+    private int maxsize = 10;
+    private int top = 0;
+    private T[] array;
+    public Stack()
+    {
+        array = new T[maxsize];
+    }
+
+    public void Push(T item)
+    {
+        this.array[top++] = item;
+    }
+
+    public T Pop()
+    {
+        return this.array[top--];
+    }
+
+    public T Peek()
+    {
+        return this.array[top - 1];
+    }
+
+    public bool IsEmpty()
+    {
+        return this.top == 0;
+    }
+
+    public bool IsFull()
+    {
+        return this.top == this.maxsize;
+    }
+
+    public Stack(int maxsize)
+    {
+        this.maxsize = maxsize;
+        array = new T[maxsize];
+    }
+
     public int add(int x, int y)
     {
         //throw new NotImplementedException("Not implemented");
