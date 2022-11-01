453c78de54993f5b3456750192de49666b7545f3
Jack Humphries <jackphumphries02@gmail.com>
2022-11-01 17:09:14 +0000

    Added `height`, `count` and `contains` methods to binary search tree

--- DataStructures.Tests/BinarySearchTreeTest.cs
+++ DataStructures.Tests/BinarySearchTreeTest.cs
@@ -3,18 +3,34 @@ using DataStructures;
 
 public class BinarySearchTreeTest
 {
-    public Node<Char> createTestTree()
+    public BinarySearchTree<int> createTestTree1()
     {
-        var root = new Node<Char>('A');
-        root.Left = new Node<Char>('B');
-        root.Right = new Node<Char>('C');
-        root.Left.Left = new Node<Char>('D');
-        root.Left.Right = new Node<Char>('E');
-        root.Right.Left = new Node<Char>('F');
-        root.Right.Right = new Node<Char>('G');
-        root.Left.Right.Right = new Node<Char>('H');
+        var tree = new BinarySearchTree<int>();
 
-        return root;
+        tree.InsertItem(21);
+        tree.InsertItem(7);
+        tree.InsertItem(30);
+        tree.InsertItem(3);
+        tree.InsertItem(9);
+        tree.InsertItem(12);
+        tree.InsertItem(24);
+        tree.InsertItem(39);
+
+        return tree;
+    }
+
+    public BinarySearchTree<int> createTestTree2()
+    {
+        var tree = new BinarySearchTree<int>();
+
+        //[12, 6, 8, 16, 19] 
+        tree.InsertItem(12);
+        tree.InsertItem(6);
+        tree.InsertItem(8);
+        tree.InsertItem(16);
+        tree.InsertItem(19);
+
+        return tree;
     }
 
     [Fact]
@@ -32,5 +48,61 @@ public class BinarySearchTreeTest
         bstree.InsertItem(6);
         bstree.InsertItem(18);
         Assert.Equal("6, 12, 18", bstree.InOrder());
+    }
+    [Fact]
+    public void TestHeight1()
+    {
+        var bstree = createTestTree1();
+
+        Assert.Equal(4, bstree.Height());
+    }
+    [Fact]
+    public void TestCount1()
+    {
+        var bstree = createTestTree1();
+
+        Assert.Equal(8, bstree.Count());
+    }
+    [Fact]
+    public void TestContains1()
+    {
+        var bstree = createTestTree1();
+
+        Assert.True(bstree.Contains(9));
+    }
+    [Fact]
+    public void TestNotContains1()
+    {
+        var bstree = createTestTree1();
+
+        Assert.False(bstree.Contains(4));
+    }
+    [Fact]
+    public void TestHeight2()
+    {
+        var bstree = createTestTree2();
+
+        Assert.Equal(3, bstree.Height());
+    }
+    [Fact]
+    public void TestCount2()
+    {
+        var bstree = createTestTree2();
+
+        Assert.Equal(5, bstree.Count());
+    }
+    [Fact]
+    public void TestContains2()
+    {
+        var bstree = createTestTree2();
+
+        Assert.True(bstree.Contains(19));
+    }
+    [Fact]
+    public void TestNotContains2()
+    {
+        var bstree = createTestTree2();
+
+        Assert.False(bstree.Contains(4));
     }
 }
--- DataStructures/BinarySearchTree.cs
+++ DataStructures/BinarySearchTree.cs
@@ -43,4 +43,98 @@ public class BinarySearchTree<T> : BinaryTree<T> where T : IComparable
             insertItem(item, ref tree.Right);
         }
     }
+
+    public int Height()
+    {
+        int h = 0;
+        int m = 1;
+        if (this.root != null)
+        {
+            this.height(this.root, ref h, ref m);
+        }
+        return m;
+    }
+
+    private int height(Node<T> node, ref int n, ref int m)
+    {
+        if (node.Left != null)
+        {
+            this.height(node.Left, ref n, ref m);
+        }
+
+        if (node.Right != null)
+        {
+            this.height(node.Right, ref n, ref m);
+        }
+
+        if (n > m)
+        {
+            m = n;
+            n = 1;
+        }
+
+        n++;
+
+        return n;
+    }
+
+    public int Count()
+    {
+        int l = 0;
+        if (this.root != null)
+        {
+            this.count(this.root, ref l);
+        }
+        return l;
+    }
+
+    private int count(Node<T> node, ref int n)
+    {
+        if (node.Left != null)
+        {
+            this.count(node.Left, ref n);
+        }
+
+        if (node.Right != null)
+        {
+            this.count(node.Right, ref n);
+        }
+
+        n++;
+
+        return n;
+    }
+
+    public bool Contains(T item)
+    {
+        bool c = false;
+        if (this.root != null)
+        {
+            this.contains(this.root, item, ref c);
+        }
+        return c;
+    }
+
+    private void contains(Node<T> node, T item, ref bool c)
+    {
+        if (c == true)
+        {
+            return;
+        }
+
+        if (item.Equals(node.Data))
+        {
+            c = true;
+        }
+
+        if (node.Left != null)
+        {
+            this.contains(node.Left, item, ref c);
+        }
+
+        if (node.Right != null)
+        {
+            this.contains(node.Right, item, ref c);
+        }
+    }
 }
